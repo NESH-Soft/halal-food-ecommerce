f1d06b91b2d591ab97adcb98c3345bfd14c02610
sheikhhasib <hasib2130@gamil.com>
2020-11-04 11:24:43 +0600

    cart page create

--- client/src/component/Cart/Cart.js
+++ client/src/component/Cart/Cart.js
@@ -10,7 +10,7 @@ const Cart = () => {
             <div className="col-md-8 m-auto">
                 <table class="table table-borderless">
                     <thead>
-                        <tr>
+                        <tr style={{ backgroundColor: '#DDDDDD' }}>
                             <th scope="col">IMAGE</th>
                             <th scope="col">PRODUCT</th>
                             <th scope="col">PRICE</th>
@@ -26,7 +26,21 @@ const Cart = () => {
                             <td>Otto</td>
                             <td>Otto</td>
                             <td>Otto</td>
-                            <td>@mdo</td>
+                            <td>
+                                <div class="input-group">
+                                    <span class="input-group-btn">
+                                        <button type="button" class="btn btn-default btn-number" disabled="disabled" data-type="minus" data-field="quant[1]">
+                                            <span class="glyphicon glyphicon-minus"></span>
+                                        </button>
+                                    </span>
+                                    <input type="text" name="quant[1]" class="form-control input-number" value="1" min="1" max="10" />
+                                    <span class="input-group-btn">
+                                        <button type="button" class="btn btn-default btn-number" data-type="plus" data-field="quant[1]">
+                                            <span class="glyphicon glyphicon-plus"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
