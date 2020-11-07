4e974a0e4754d4c870f85ae45ddb7c041857dbc4
Abdullah Naser <abdnaser.exe@gmail.com>
2020-11-07 16:31:44 +0600

    Category route added

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,11 @@
 import userRouteConfigure from './userRoutes';
 import productRouteConfigure from './productRoute';
+import categoryRouteConfigure from './categoryRoutes';
 
 const configureAllRoutes = (app) => {
   userRouteConfigure(app);
   productRouteConfigure(app);
+  categoryRouteConfigure(app);
 };
 
 export default configureAllRoutes;
