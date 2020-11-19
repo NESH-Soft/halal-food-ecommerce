222d0009f3195c54089090d012f0964ad63a618c
sheikhhasib <hasib2130@gamil.com>
2020-11-19 12:39:25 +0600

    login and registration react hook form use

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,8 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "react-multi-carousel/lib/styles.css";
-
 import store from '../src/redux/store'
+
 
 ReactDOM.render(
   <React.StrictMode>
