8cdb02d2d4ca7b4b80e0e9d4d2c81853540328fb
sheikhhasib <hasib2130@gamil.com>
2020-11-20 22:31:14 +0600

    update contact design

--- client/src/component/MyAccount/MyAccount.js
+++ client/src/component/MyAccount/MyAccount.js
@@ -1,62 +1,52 @@
 import React from 'react'
 import './style.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGift ,faLock,faLocationArrow } from '@fortawesome/free-solid-svg-icons'
+
 const MyAccount = () => {
-  return (
-    <div>
-       <div class="my-account-box-main">
-        <div class="container">
-            <div class="my-account-page">
-                <div class="row">
-                    <div class="col-lg-4 col-md-12">
-                        <div class="account-box">
-                            <div class="service-box">
-                                <div class="service-icon">
-                                    <a href="#"> <i class="fa fa-gift"></i> </a>
-                                </div>
-                                <div class="service-desc">
-                                    <h4>Your Orders</h4>
-                                    <p>Track, return, or buy things again</p>
-                                </div>
-                            </div>
+    return (
+        <div className="row">
+            <div className="col-lg-4 col-md-12">
+                <div className="account-box">
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <a href="#"> <FontAwesomeIcon icon={faGift} style={{fontSize:'50px',color:'#fff'}} /> </a>
+                        </div>
+                        <div className="service-desc">
+                            <h4>Your Orders</h4>
+                            <p>Track, return, or buy things again</p>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-12">
-                        <div class="account-box">
-                            <div class="service-box">
-                                <div class="service-icon">
-                                    <a href="#"><i class="fa fa-lock"></i> </a>
-                                </div>
-                                <div class="service-desc">
-                                    <h4>Login &amp; security</h4>
-                                    <p>Edit login, name, and mobile number</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-12">
-                        <div class="account-box">
-                            <div class="service-box">
-                                <div class="service-icon">
-                                    <a href="#"> <i class="fa fa-location-arrow"></i> </a>
-                                </div>
-                                <div class="service-desc">
-                                    <h4>Your Addresses</h4>
-                                    <p>Edit addresses for orders and gifts</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-             
-                
                 </div>
-
-
-
+            </div>
+            <div className="col-lg-4 col-md-12">
+                <div className="account-box">
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <a href="#"> <FontAwesomeIcon icon={faLock} style={{fontSize:'50px',color:'#fff'}} /> </a>
+                        </div>
+                        <div className="service-desc">
+                            <h4>Login &amp; security</h4>
+                            <p>Edit login, name, and mobile number</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="col-lg-4 col-md-12">
+                <div className="account-box">
+                    <div className="service-box">
+                        <div className="service-icon">
+                            <a href="#"> <FontAwesomeIcon icon={faLocationArrow} style={{fontSize:'50px',color:'#fff'}} /> </a>
+                        </div>
+                        <div className="service-desc">
+                            <h4>Your Addresses</h4>
+                            <p>Edit addresses for orders and gifts</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-  )
+    )
 }
 
 export default MyAccount
