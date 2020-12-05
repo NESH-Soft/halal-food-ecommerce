06119f618f6982dbf659c9d9b17b47070fdf903c
Abdullah Naser <abdnaser.exe@gmail.com>
2020-12-05 13:38:47 +0600

    Ready for deployment

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import configureAllRoutes from './routes/index';
 import { handleError } from './middlewares/index';
@@ -9,6 +10,15 @@ dotenv.config({ path: './config/config.env' });
 const app = express();
 app.use(express.json());
 configureAllRoutes(app);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'PRODUCTION') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use(handleError);
 
