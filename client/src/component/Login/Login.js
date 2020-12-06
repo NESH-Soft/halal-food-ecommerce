0c6cf4759ff6dd7e6f24b42567968871041fb05e
sheikhhasib <hasib2130@gamil.com>
2020-12-06 13:47:27 +0600

    update productDetails and login register page

--- client/src/component/Login/Login.js
+++ client/src/component/Login/Login.js
@@ -1,59 +1,58 @@
-import React,{useEffect} from 'react'
-import { withRouter,Link } from "react-router-dom"
+import React, { useEffect } from 'react'
+import { withRouter, Link } from "react-router-dom"
 import { useForm } from "react-hook-form"
 import { useDispatch, useSelector } from 'react-redux'
-import {signIn} from '../../redux/actions/authAction'
+import { signIn } from '../../redux/actions/authAction'
 import './Login.css'
 
 const Login = (props) => {
     const dispatch = useDispatch();
     const isAuthenticated = useSelector((state) => state.authState.isAuthenticated);
     useEffect(() => {
-       
-        if(isAuthenticated){
-          props.history.push('/my-account');
+
+        if (isAuthenticated) {
+            props.history.push('/my-account');
         }
         // eslint-disable-next-line
-      },[isAuthenticated])
+    }, [isAuthenticated])
 
-  
+
     const { register, handleSubmit, watch, errors } = useForm();
     const onSubmit = data => {
-    dispatch(signIn(data))
+        dispatch(signIn(data))
     }
     return (
         <div className="login">
-            <div className="row d-flex justify-content-center align-items-center">
-                <div className="col-md-3">
-                    <div class="card mb-4 shadow-lg p-3 bg-white" style={{ width: '100%' }}>
-                        <div class="card-body">
-                            <h3 class="card-title text-center">LOGIN</h3>
-                            <div className="mt-5">
-                                <form onSubmit={handleSubmit(onSubmit)}>
-                                    <div class="form-group">
-                                        <label for="exampleInputEmail1">Email address</label>
-                                        <input type="email" name="email" ref={register({ required: true })} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                                        {errors.email && <span>This field is required</span>}
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleInputPassword1">Password</label>
-                                        <input type="password" name="password" ref={register({ required: true })} class="form-control" id="exampleInputPassword1" placeholder="Password" />
-                                        {errors.password && <span>This field is required</span>}
-                                    </div>
+            <div className="row">
+                <div className="col-md-3"></div>
+                <div className="col-md-7 mb-5">
+                    <div style={{ borderBottom: '3px solid #76a333' }}>
+                        <h3 class="card-title">Login Your Account</h3>
+                    </div>
+                    <div className="col-md-6 m-auto">
+                        <div className="mt-5">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail1">Email address</label>
+                                    <input type="email" name="email" ref={register({ required: true })} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                                    {errors.email && <span>This field is required</span>}
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword1">Password</label>
+                                    <input type="password" name="password" ref={register({ required: true })} class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                    {errors.password && <span>This field is required</span>}
+                                </div>
 
-                                    <div className="form-froup">
-                                        <button type="submit" class="btn btn-primary w-100">Submit</button>
-                                    </div>
-                                </form>
-                            </div>
-                            <br />
-                            <p className="text-center">Dont't have an acceount ?<Link to="/register" class="card-link">Sign Up</Link> </p>
-
+                                <div className="form-froup">
+                                    <button type="submit" class="btn btn-primary w-100">Submit</button>
+                                </div>
+                            </form>
                         </div>
+                        <br />
+                        <p className="text-center">Dont't have an acceount ?<Link to="/register" class="card-link">Sign Up</Link> </p>
                     </div>
                 </div>
             </div>
-
         </div>
     );
 };
