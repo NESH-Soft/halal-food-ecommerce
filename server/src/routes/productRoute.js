6a11c4b3504ab81791667d11244d2252f08ef790
shohedul350 <shohedul350@gmail.com>
2020-12-07 22:41:10 +0600

    bug fixed

--- server/src/routes/productRoute.js
+++ server/src/routes/productRoute.js
@@ -16,9 +16,9 @@ const router = express.Router();
 router.route('/search').get(searchProduct);
 router.route('/special-product').get(specialProduct);
 router.route('/filter').get(getProductsByCategory);
-router.route('/').get(getProducts).post(auth.protect, auth.authorize('admin'), multerUpload.single('image'), addProduct);
+router.route('/').get(getProducts).post(multerUpload.single('image'), addProduct);
 
-router.route('/:id').get(getProduct).delete(auth.protect, auth.authorize('admin'), deleteProduct).put(updateProduct);
+router.route('/:id').get(getProduct).delete(deleteProduct).put(updateProduct);
 
 const configure = (app) => {
   app.use('/api/product', router);
