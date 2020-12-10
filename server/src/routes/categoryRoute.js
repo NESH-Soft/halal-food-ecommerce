20b0f4ced0ecfbdadf3b404d2d4533c759ec59ec
Abdullah Naser <abdnaser.exe@gmail.com>
2020-12-10 17:43:34 +0600

    Two url params added for deleting sub category

--- server/src/routes/categoryRoute.js
+++ server/src/routes/categoryRoute.js
@@ -11,7 +11,7 @@ import {
 const router = express.Router();
 
 router.route('/').get(getCategory).post(auth.protect, addCategory);
-router.route('/subcategory-delete/:id').put(auth.protect, deleteSubCategory);
+router.route('/subcategory-delete/:CatId/:subCatId').put(auth.protect, deleteSubCategory);
 router.route('/:id').delete(auth.protect, deleteCategory).put(auth.protect, addSubCategory);
 const configure = (app) => {
   app.use('/api/category', router);
