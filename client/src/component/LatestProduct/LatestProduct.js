71c09cda7c637396509bdc09820bb44ccbba5f18
shohedul350 <shohedul350@gmail.com>
2020-12-11 19:48:22 +0600

    latest product fetch

--- client/src/component/LatestProduct/LatestProduct.js
+++ client/src/component/LatestProduct/LatestProduct.js
@@ -1,56 +1,79 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus, faMinus } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeCart } from '../../redux/actions/cartAction';
 import './LetestProduct.css'
 
 const LetestProduct = () => {
+    const dispatch = useDispatch()
+    const latestProducts = useSelector((state) => state.productState.latestProducts);
+    const cartItem = useSelector((state) => state.cartState.cart);
+  
+
+    // include all productId from cart state
+    const cartItemArray = cartItem.map(function (product) {
+        return product._id
+    });
+
     return (
         <div className="card p-2 mt-5">
             <div style={{ backgroundColor: '#76a333',borderRadius: '5px' }} className="p-2 mb-3 text-light">
                 <h5>Latest Products</h5>
             </div>
-            <div className="row">
+
+
+            {
+               latestProducts && latestProducts.map((pd,index)=>(
+                   <div key={index}>
+                <div className="row">
                 <div className="col-md-5">
-                    <img src="https://res.cloudinary.com/halal-food/image/upload/v1606999541/fxtjvmqh7ojzifwx6miq.jpg" width="100%" height="120px" alt="" />
-                </div>
-                <div className="col-md-7 pl-3">
-                    <p>Dry lolitta fish (100g)</p>
-                    <p>¥515</p>
-                    <button className="btn btn-sm btn-Addtocart btn-color btn-padding">Add Item</button>
-                </div>
-            </div>
-            <hr />
-            <div className="row">
-                <div className="col-md-5">
-                    <img src="https://res.cloudinary.com/halal-food/image/upload/v1607163458/igqjtkdrfroy2rsbpksl.jpg" width="100%" height="120px" alt="" />
-                </div>
-                <div className="col-md-7 pl-3">
-                    <p>Dry cake (Pran 350g)</p>
-                    <p>¥398</p>
-                    <button className="btn btn-sm btn-Addtocart btn-color btn-padding">Add Item</button>
-                </div>
-            </div>
-            <hr />
-            <div className="row">
-                <div className="col-md-5">
-                    <img src="https://res.cloudinary.com/halal-food/image/upload/v1607662265/yfnbvpexj1brofmdxhse.jpg" width="100%" height="120px" alt="" />
-                </div>
-                <div className="col-md-7 pl-3">
-                    <p>Dry lolitta fish (100g)</p>
-                    <p>¥1570</p>
-                    <button className="btn btn-sm btn-Addtocart btn-color btn-padding">Add Item</button>
-                </div>
-            </div>
-            <hr />
-            <div className="row">
-                <div className="col-md-5">
-                    <img src="https://res.cloudinary.com/halal-food/image/upload/v1607661674/gmicgwsbjjp6ehzp1edn.jpg" width="100%" height="120px" alt="" />
-                </div>
-                <div className="col-md-7 pl-3">
-                    <p>Fruits Incense sticks</p>
-                    <p>¥108</p>
-                    <button className="btn btn-sm btn-Addtocart btn-color btn-padding">Add Item</button>
-                </div>
-            </div>
-            <hr />
+                       <img src={pd.image} width="100%" height="120px" alt="" />
+                   </div>
+                   <div className="col-md-7 pl-3">
+                       <p>{pd.name}</p>
+                       <p>{pd.specialPrice}</p>
+
+                       {
+
+cartItemArray.includes(pd._id) ? (
+    <button
+
+        disabled={
+            pd.stock <= 0
+        }
+        className="btn btn-sm btn-Addtocart btn-color px-3"
+        onClick={() => dispatch(removeCart(pd._id))}
+    >
+        <FontAwesomeIcon icon={faMinus} /> Remove item
+    </button>
+) : (
+        <button
+
+            disabled={
+                pd.stock <= 0
+            }
+
+            className="btn btn-sm btn-Addtocart btn-color px-3"
+
+            onClick={() => dispatch(addToCart(pd))}
+        >
+            <FontAwesomeIcon icon={faCartPlus} /> Add item
+        </button>
+    )
+
+}
+
+                   </div>
+               </div>
+               <hr />
+               </div>
+               )) 
+            }
+          
+    
+          
+           
 
         </div>
     );
