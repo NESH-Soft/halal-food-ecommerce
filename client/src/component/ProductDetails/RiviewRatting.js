1fc323ac34205302028ae8701fc526bdf8e5dced
shohedul350 <shohedul350@gmail.com>
2020-12-16 15:07:58 +0600

    rating fatch in review component

--- client/src/component/ProductDetails/RiviewRatting.js
+++ client/src/component/ProductDetails/RiviewRatting.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ProductDetails.css';
 const RiviewRatting = (props) => {
-const data = props.review || []
+const data = props.rating || []
     const ratingArray = data.map(function (rat) {
         return rat.rating 
     });
@@ -11,17 +11,20 @@ const data = props.review || []
     }, 0);
 
 
-    const rating1 = ratingArray.filter(rating => rating === 1)
 
-    const rating2 = ratingArray.filter(rating => rating === 2)
+    const calculatePresentence=(ratingNumber)=>{
+        const findRatingNumber = ratingArray.filter(rating => rating === ratingNumber)
+        const countRatingNumber = findRatingNumber.reduce(function (accumulator, currentValue) {
+            return accumulator + currentValue;;
+        }, 0);
 
-    const rating3 = ratingArray.filter(rating => rating === 3)
+        return (totalRating / 100) * countRatingNumber;
+    
+    }
 
-    const rating4 = ratingArray.filter(rating => rating === 4)
-    const rating5 = ratingArray.filter(rating => rating === 5)
 
     
-    console.log(rating1,rating2,rating3,rating4,rating5)
+
     return (
         <div>
             <div className="row">
@@ -30,7 +33,7 @@ const data = props.review || []
                         <div className="row">
                             <div className="col-md-3">
                                 <h1 className="rating-num">
-                                    4.0</h1>
+                                    5.0</h1>
                                 <div className="rating">
                                     <span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star">
                                     </span><span className="glyphicon glyphicon-star"></span><span className="glyphicon glyphicon-star">
@@ -48,8 +51,8 @@ const data = props.review || []
                                     <div className="col-xs-8 col-md-9">
                                         <div className="progress progress-striped">
                                             <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
-                                                aria-valuemin="0" aria-valuemax="100" style={{ width: '80%' }}>
-                                                <span className="sr-only">80%</span>
+                                                aria-valuemin="0" aria-valuemax="100" style={{ width: `${calculatePresentence(5)}%`}}>
+                                                <span className="sr-only">{calculatePresentence(5)}%</span>
                                             </div>
                                         </div>
                                     </div>
@@ -58,9 +61,9 @@ const data = props.review || []
                                     </div>
                                     <div className="col-xs-8 col-md-9">
                                         <div className="progress">
-                                            <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
-                                                aria-valuemin="0" aria-valuemax="100"style={{ width: '60%' }}>
-                                                <span className="sr-only">60%</span>
+                                        <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                                aria-valuemin="0" aria-valuemax="100" style={{ width: `${calculatePresentence(4)}%`}}>
+                                                <span className="sr-only">{calculatePresentence(4)}%</span>
                                             </div>
                                         </div>
                                     </div>
@@ -69,9 +72,9 @@ const data = props.review || []
                                     </div>
                                     <div className="col-xs-8 col-md-9">
                                         <div className="progress">
-                                            <div className="progress-bar progress-bar-info" role="progressbar" aria-valuenow="20"
-                                                aria-valuemin="0" aria-valuemax="100" style={{ width: '40%' }}>
-                                                <span className="sr-only">40%</span>
+                                        <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                                aria-valuemin="0" aria-valuemax="100" style={{ width: `${calculatePresentence(3)}%`}}>
+                                                <span className="sr-only">{calculatePresentence(3)}%</span>
                                             </div>
                                         </div>
                                     </div>
@@ -80,9 +83,9 @@ const data = props.review || []
                                     </div>
                                     <div className="col-xs-8 col-md-9">
                                         <div className="progress">
-                                            <div className="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="20"
-                                                aria-valuemin="0" aria-valuemax="100" style={{ width: '20%' }}>
-                                                <span className="sr-only">20%</span>
+                                        <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                                aria-valuemin="0" aria-valuemax="100" style={{ width: `${calculatePresentence(2)}%`}}>
+                                                <span className="sr-only">{calculatePresentence(2)}%</span>
                                             </div>
                                         </div>
                                     </div>
@@ -91,9 +94,9 @@ const data = props.review || []
                                     </div>
                                     <div className="col-xs-8 col-md-9">
                                         <div className="progress">
-                                            <div className="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="80"
-                                                aria-valuemin="0" aria-valuemax="100" style={{ width: '15%' }}>
-                                                <span className="sr-only">15%</span>
+                                        <div className="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20"
+                                                aria-valuemin="0" aria-valuemax="100" style={{ width: `${calculatePresentence(1)}%`}}>
+                                                <span className="sr-only">{calculatePresentence(1)}%</span>
                                             </div>
                                         </div>
                                     </div>
