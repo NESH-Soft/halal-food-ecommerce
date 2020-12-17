55ce2f2dbc0ee160c3c9751445e0f65cfeaf9b51
sheikhhasib <hasib2130@gamil.com>
2020-12-17 07:21:18 +0600

    user comment update

--- client/src/component/ProductDetails/UserComment/UserComment.js
+++ client/src/component/ProductDetails/UserComment/UserComment.js
@@ -1,21 +1,70 @@
 import React from 'react';
 import './UserComment.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faStar } from '@fortawesome/free-solid-svg-icons'
 
-const UserComment = ({review}) => {
+const UserComment = ({ review }) => {
     const data = review || []
     return (
-        <div className="col-md-12">
+        <div>
             <p className="py-2 pl-2 comment-header">Customer review ({data.length})</p>
             <div>
-               {data.map(rv =>(
+                {data.map(rv => (
 
-                  <div>
-                    <p> {rv.customerName}</p>
+                    <div>
+                        {/* <p> {rv.customerName}</p>
                     <p> {rv.rating}</p> 
-                    <p> {rv.review}</p> 
-                    <hr/>
-                  </div>
-               ))}
+                    <p> {rv.review}</p>  */}
+                        <div className="d-flex">
+                            <div style={{backgroundColor:'lightgray',borderRadius:'50%',padding:'5px 10px',marginRight:'10px'}}>
+                                <FontAwesomeIcon className="text-secondary" icon={faUser} />
+                            </div>
+                            <h6 className="pt-1"> {rv.customerName}</h6>
+                        </div>
+                        <div>
+                            {
+                                rv.rating === 5 && <div>
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                </div>
+                            }
+                            {
+                                rv.rating === 4 && <div>
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                </div>
+                            }
+                            {
+                                rv.rating === 3 && <div>
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                </div>
+                            }
+                            {
+                                rv.rating === 2 && <div>
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                </div>
+                            }
+                            {
+                                rv.rating === 1 && <div>
+                                    <FontAwesomeIcon className="text-warning" icon={faStar} />
+                                </div>
+                            }
+                        </div>
+                        <div>
+                            <p> {rv.review}</p>
+                        </div>
+
+                        <hr />
+                    </div>
+                ))}
             </div>
         </div>
     );
